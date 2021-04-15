5d3f087297164670b0c74390d421066120c0449d
CyganFx <duman070601@gmail.com>
2021-04-15 23:04:10 +0600

    first commit

---
+++ comparison_survey/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'date_posted': 'August 27'
+    },
+    {
+        'author': 'asd2',
+        'title': 'Blog Post 2',
+        'date_posted': 'August 28'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'comparison_survey/home.page.html', context)
