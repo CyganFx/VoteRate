be7d51846988a39fc8d0ede5e049c95b26bc7e34
Magauiya Mazhit <m.mazhit@astanait.edu.kz>
2021-04-26 16:53:04 +0600

    comparison_survey app: preparation for build

--- comparison_survey/urls.py
+++ comparison_survey/urls.py
@@ -2,8 +2,25 @@ from django.urls import path
 from comparison_survey.views import *
 
 
+'''
+we are here voterate.com/comparison_survey
+
+retrieve 
+    all surveys - / # available for any user
+    survey by id - /<int:id> # available for any user
+    survey(s)
+                by top number - /search/<int:_num>
+                by topic - /search/<str:_topic>
+                by date - /search/<int:year>/<int:month> # ?(shows starting from that date) 
+
+# comparison surveys can be modified only by the creator of surveys 
+create new survey - /edit # below urls available for registered users
+update survey - /edit/id
+delete survey - /edit/id
+'''
+
 urlpatterns = [
-    path('', home, name='comparison_survey-home'),
-    # path('survey/<int:id>/', views.get_c_survey_by_id, name='comparison-survey-by-id'),
+    path('', Retrieve_ListView, name='comparison_survey-home'),
+    path('<int:id>', Retrieve_DetailView, name='comparison_survey-by-id'),
 ]
 
--- comparison_survey/views.py
+++ comparison_survey/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.shortcuts import render
 
-from comparison_survey.models import ComparisonSurvey
-
-
-def home(request):
-    all_surveys = ComparisonSurvey.objects.order_by('rating')
-    print(f"All surveys: {all_surveys}")
-    return render(request, 'comparison_survey/home.page.html', {'all_surveys': all_surveys})
+from .models import ComparisonSurvey
 
 
-def get_c_survey_by_id(request, id):
-    c_survey = get_object_or_404(ComparisonSurvey, pk=id)
-    return render(request, 'comparison_survey/home.page.html', {c_survey.id: c_survey})
+def Retrieve_ListView(request):
+    dataset = ComparisonSurvey.objects.all()
+    return render(request, 'comparison_survey/home.page.html', {'dataset': dataset})
+
+
+def Retrieve_DetailView(request, id):
+    try:
+        survey = ComparisonSurvey.objects.get(id=id)
+    except ComparisonSurvey.DoesNotExist:
+        raise Http404('Data does not exist')
+
+    return render(request, 'comparison_survey/survey.create.html', {'survey': survey})
 
